6263730d5add22e47350ea6f262afaf70a51327a
Rizanoor <rizanoorfauzan123@gmail.com>
2023-05-18 05:21:55 +0700

    feat: add models

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable =
+    [
+        'name', 'users_id', 'price', 'description', 'discount_percentage', 'slug'
+    ];
+
+    public function galleries()
+    {
+        return $this->hasMany(ProductGallery::class, 'products_id', 'id');
+    }
+
+    public function user()
+    {
+        return $this->hasOne(User::class, 'id', 'users_id');
+    }
 }
--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Transaction extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'users_id',
+        'total_price',
+        'transaction_status',
+    ];
+
+    protected $hidden = [];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'users_id', 'id');
+    }
 }
