76191aa514375800d6204482b0a0362996b2dfb9
Rizanoor <rizanoorfauzan123@gmail.com>
2023-05-18 06:53:51 +0700

    feat: edit n delete Product

---
+++ resources/views/pages/admin/product/edit.blade.php
@@ -0,0 +1,94 @@
+@extends('layouts.admin')
+
+@section('title')
+    Product Edit Store Dashboard
+@endsection
+
+@section('content')
+<!-- Section Content -->
+<div
+    class="section-content section-dashboard-home"
+    data-aos="fade-up"
+    >
+    <div class="container-fluid">
+        <div class="dashboard-heading">
+            <h2 class="dashboard-title">Product</h2>
+            <p class="dashboard-subtitle">
+              Edit "{{ $item->name }}" Product
+            </p>
+        </div>
+        <div class="dashboard-content">
+            <div class="row">
+                <div class="col-md-12">
+                  @if ($errors->any())
+                      <div class="alert alert-danger">
+                        <ul>
+                          @foreach ($errors->all() as $error)
+                              <li>{{ $error }}</li>
+                          @endforeach
+                        </ul>
+                      </div>
+                  @endif
+                    <div class="card">
+                        <div class="card-body">
+                            <form action="{{ route('product.update', $item->id) }}" method="POST" enctype="multipart/form-data">
+                            @method('PUT')
+                            @csrf
+                            <div class="row">
+                              <div class="col md-12">
+                                <div class="form-group">
+                                  <label>Nama Product</label>
+                                  <input type="text" name="name" class="form-control" value="{{ $item->name }}" required>
+                                </div>
+                                  <div class="form-group">
+                                    <label>Pemilik Product</label>
+                                      <select name="users_id" class="form-control">
+                                          <option value="{{ $item->users_id }}" >{{ $item->user->name }}</option>
+                                        @foreach ($users as $user)
+                                          <option value="{{ $user->id }}">{{ $user->name }}</option>
+                                        @endforeach
+                                      </select>
+                                  </div>
+                                  <div class="form-group">
+                                    <label>Harga Product</label>
+                                    <input type="number" name="price" class="form-control" value="{{ $item->price }}" required>
+                                  </div>
+                                  <div class="form-group">
+                                    <label>Diskon Persenan</label>
+                                    <input type="number" name="discount_percentage" class="form-control" value="{{ $item->discount_percentage }}" min="1" max="100">
+                                  </div>
+                                  <div class="form-group">
+                                    <label>Deskripsi Product</label>
+                                    <textarea name="description" id="editor" cols="30" rows="10">{!! $item->description !!}</textarea>
+                                  </div>
+                              </div>
+                            </div>
+                              <div class="row">
+                                <div class="col text-right">
+                                  <button type="submit" class="btn btn-success px-5">
+                                    Save Now</button>
+                                </div>
+                              </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+@push('addon-script')
+  <script src="https://cdn.ckeditor.com/ckeditor5/31.1.0/classic/ckeditor.js"></script>
+  <script>
+    ClassicEditor
+            .create( document.querySelector( '#editor' ) )
+            .then( editor => {
+                    console.log( editor );
+            } )
+            .catch( error => {
+                    console.error( error );
+            } );
+  </script>
+@endpush
