0e1cb853af42c3fc410cdda51f19de9dc53f456e
Rizanoor <rizanoorfauzan123@gmail.com>
2023-05-18 09:25:06 +0700

    feat: list user Dashboard

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\User;
+use App\Http\Requests\Admin\UserRequest;
+
+use Yajra\DataTables\Facades\DataTables;
 class UserController extends Controller
 {
     /**
@@ -14,7 +18,40 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        if (request()->ajax()) {
+            $query = User::query();
+
+            return Datatables::of($query)
+                ->addColumn('action', function ($item) {
+                    return '
+                        <div class="btn-group">
+                            <div class="dropdown">
+                                <button class="btn btn-primary dropdown-toggle mr-1 mb-1"
+                                    type="button" id="action' .  $item->id . '"
+                                        data-toggle="dropdown"
+                                        aria-haspopup="true"
+                                        aria-expanded="false">
+                                        Aksi
+                                </button>
+                                <div class="dropdown-menu" aria-labelledby="action' .  $item->id . '">
+                                    <a class="dropdown-item" href="' . route('user.edit', $item->id) . '">
+                                        Edit
+                                    </a>
+                                    <form action="' . route('user.destroy', $item->id) . '" method="POST">
+                                        ' . method_field('delete') . csrf_field() . '
+                                        <button type="submit" class="dropdown-item text-danger">
+                                            Hapus
+                                        </button>
+                                    </form>
+                                </div>
+                            </div>
+                    </div>';
+                })
+                ->rawColumns(['action', 'photo'])
+                ->make();
+        }
+
+        return view('pages.admin.user.index');
     }
 
     /**
@@ -57,7 +94,11 @@ class UserController extends Controller
      */
     public function edit($id)
     {
-        //
+        $item = User::findOrFail($id);
+
+        return view('pages.admin.user.edit', [
+            'item' => $item
+        ]);
     }
 
     /**
@@ -67,9 +108,21 @@ class UserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(UserRequest $request, $id)
     {
-        //
+        $data = $request->all();
+
+        $item = User::findOrFail($id);
+
+        if ($request->password) {
+            $data['password'] =  bcrypt($request->password);
+        } else {
+            unset($data['password']);
+        }
+
+        $item->update($data);
+
+        return redirect()->route('user.index');
     }
 
     /**
@@ -80,6 +133,9 @@ class UserController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $item = User::findOrFail($id);
+        $item->delete();
+
+        return redirect()->route('user.index');
     }
 }
