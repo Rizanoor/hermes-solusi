ab3dc9c7debb38ed3873c36298066718987db7da
Rizanoor <rizanoorfauzan123@gmail.com>
2023-05-18 09:09:43 +0700

    feat: create n delete ProductGallery

--- app/Http/Controllers/Admin/ProductGalleryController.php
+++ app/Http/Controllers/Admin/ProductGalleryController.php
@@ -2,8 +2,14 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\ProductGallery;
+use App\Http\Controllers\Controller;
+use Yajra\DataTables\Facades\DataTables;
+use Illuminate\Support\Facades\Storage;
+use App\Models\Product;
+use App\Http\Requests\Admin\ProductGalleryRequest;
+
 
 class ProductGalleryController extends Controller
 {
@@ -14,7 +20,41 @@ class ProductGalleryController extends Controller
      */
     public function index()
     {
-        //
+        if (request()->ajax()) {
+            $query = ProductGallery::with(['product']);
+
+            return Datatables::of($query)
+                ->addColumn('action', function ($item) {
+                    return '
+                        <div class="btn-group">
+                            <div class="dropdown">
+                                <button class="btn btn-primary dropdown-toggle mr-1 mb-1"
+                                    type="button" id="action' .  $item->id . '"
+                                        data-toggle="dropdown"
+                                        aria-haspopup="true"
+                                        aria-expanded="false">
+                                        Aksi
+                                </button>
+                                <div class="dropdown-menu" aria-labelledby="action' .  $item->id . '">
+                                    <form action="' . route('product-gallery.destroy', $item->id) . '" method="POST">
+                                        ' . method_field('delete') . csrf_field() . '
+                                        <button type="submit" class="dropdown-item text-danger">
+                                            Hapus
+                                        </button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    ';
+                })
+                ->editColumn('photos', function ($item) {
+                    return $item->photos ? '<img src= "' . Storage::url($item->photos) . '" style="max-height:80px;" />' : '';
+                })
+                ->rawColumns(['action', 'photos'])
+                ->make();
+        }
+
+        return view('pages.admin.product-gallery.index');
     }
 
     /**
@@ -24,7 +64,11 @@ class ProductGalleryController extends Controller
      */
     public function create()
     {
-        //
+        $products = Product::all();
+
+        return view('pages.admin.product-gallery.create', [
+            'products' => $products
+        ]);
     }
 
     /**
@@ -33,9 +77,15 @@ class ProductGalleryController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ProductGalleryRequest $request)
     {
-        //
+        $data = $request->all();
+
+        $data['photos'] = $request->file('photos')->store('assets/product', 'public');
+
+        ProductGallery::create($data);
+
+        return redirect()->route('product-gallery.index');
     }
 
     /**
@@ -80,6 +130,9 @@ class ProductGalleryController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $item = ProductGallery::findOrFail($id);
+        $item->delete();
+
+        return redirect()->route('product-gallery.index');
     }
 }
